dd696fd360698b7c0dc3c289d7676a3277e7c928
Adam Rasheed <adamrasheed91@gmail.com>
2020-02-16 19:45:16 -0800

    updating ratingform

--- src/components/RatingForm/RatingForm.test.js
+++ src/components/RatingForm/RatingForm.test.js
@@ -1,37 +1,59 @@
 import React from 'react'
 import '@testing-library/jest-dom'
-import { render, fireEvent, cleanup, wait } from '@testing-library/react'
+import { render, fireEvent, cleanup, act } from '@testing-library/react'
 import RatingForm, { handleFormSubmit } from './index'
 import { mockStoreFormFilled } from '../../constants/mock'
 import SiteProvider from '../../store/SiteContext'
-import { get } from 'https'
+import { updateQuote } from '../../store/actions'
+
+// jest.mock('../../store/actions')
 
 // TODO: Add Tests
 afterEach(cleanup)
 
 // Submitting Form with Correct information should send a POST request
 test('submits POST request when form is submitted', async () => {
-  const { getByLabelText, getByTestId } = render(
-    <SiteProvider>
-      <RatingForm state={mockStoreFormFilled} />
-    </SiteProvider>
-  )
-  const Form = getByTestId('rating-form')
-  const onSubmit = jest.fn()
+  const returnedData = {}
+  const fetchRequest = jest.fn(() => {
+    return {
+      json: () => Promise.resolve(returnedData)
+    }
+  })
+  jest.spyOn(global, "fetch").mockImplementation(fetchRequest);
+
+  let container = {}
+
+
+  act(() => {
+    const { getByLabelText, getByTestId } = render(
+      <SiteProvider>
+        <RatingForm state={mockStoreFormFilled} />
+      </SiteProvider>
+    )
+    container.getByLabelText = getByLabelText
+    container.getByTestId = getByTestId
+  })
+
+  const Form = container.getByTestId('rating-form')
+
   // fill out the form
-  // fireEvent.change(getByTestId('first_name'), { target: { value: 'Adam' } })
-  // fireEvent.change(getByTestId('last_name'), { target: { value: 'Rasheed' } })
-  // fireEvent.change(getByTestId('line_1'), { target: { value: '17107 Telmo' } })
-  // fireEvent.change(getByTestId('city'), { target: { value: 'Irvine' } })
-  // fireEvent.select(getByTestId('region'), { target: { value: 'CA' } })
-  // fireEvent.change(getByTestId('postal'), { target: { value: '92618' } })
+  fireEvent.change(container.getByTestId('first_name'), { target: { value: 'Adam' } })
+  // fireEvent.change(container.getByTestId('last_name'), { target: { value: 'Rasheed' } })
+  // fireEvent.change(container.getByTestId('line_1'), { target: { value: '17107 Telmo' } })
+  // fireEvent.change(container.getByTestId('city'), { target: { value: 'Irvine' } })
+  // fireEvent.select(container.getByTestId('region'), { target: { value: 'CA' } })
+  // fireEvent.change(container.getByTestId('postal'), { target: { value: '92618' } })
 
-
-  wait(
+  act(() => {
     fireEvent.submit(Form)
-  )
-  expect(onSubmit).toHaveBeenCalledTimes(1)
-  expect(getByTestId('spinner')).toBeInTheDocument()
+  })
+  // expect(container.getByTestId('spinner')).toBeInTheDocument()
+
+  expect(fetchRequest).toHaveBeenCalledTimes(1)
+
+  // expect(updateQuote).toHaveBeenCalled()
+
+  // expect updstequote to get called with returnedData ==> 
 
 
 })
